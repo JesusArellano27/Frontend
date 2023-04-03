4a948b3eb083148a398101fc93981cc3865b9f93
JesusArellano27 <jesusarellano27091@gmail.com>
2023-04-02 23:49:48 -0600

    Visualización de salas reservadas

---
+++ src/app/interfaces/SalasR.ts
@@ -0,0 +1,11 @@
+export interface SalasR{
+    idsala:number;
+    estado:number;
+    diareservacion:String;
+    horainicio:number;
+    minutoinicio:number;
+    horafin:number;
+    minutofin:number;
+    npersonas:number;
+    cliente:String;
+}
--- src/app/verreservaciones/verreservaciones.component.ts
+++ src/app/verreservaciones/verreservaciones.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SalasR } from '../interfaces/SalasR';
+
 
 @Component({
   selector: 'app-verreservaciones',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerreservacionesComponent implements OnInit {
 
-  constructor() { }
+  API_ENDPOINT ='http://127.0.0.1:8000/api';
+
+  Salas!: SalasR[];
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get(this.API_ENDPOINT + '/versalasreservadas').subscribe((data:any)=>{
+      this.Salas = data;
+      console.log(data);
+    });
+  }
 
   ngOnInit(): void {
   }
